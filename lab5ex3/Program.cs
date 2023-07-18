7045ae2a5f76757a3e9119f9e2fa2b94eaaf59bf
Cristi Sandu <sandu.cristi@yahoo.com>
2023-07-18 20:16:43 +0300

    Add project files.

---
+++ lab5ex3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care va numara toate aparitiile unui caracter intr-un sir de caractere. Atat caracterul cautat cat si sirul de caracter vor fi citite de la tastatura.*/
+
+
+            Console.WriteLine("Introduceti sirul de caractere: ");
+            string text = Console.ReadLine();
+
+
+            Console.WriteLine("Introduceti caracterul de cautat: ");
+            char textDeCautat = char.Parse(Console.ReadLine());
+             
+            Console.WriteLine("\n"+ $"Caracterul '{textDeCautat}' este contorizat de" + " " + (ContorCaractere(text, textDeCautat)) + " "+ "ori.");
+        }
+
+            static int ContorCaractere(string text, char textCautat)
+            {
+                int contorCaractere = 0;
+                foreach (char c in text)
+                {
+                    if (textCautat==c)
+                    {
+                        contorCaractere ++;
+                    }
+                }
+
+                return contorCaractere; 
+            }
+
+            
+    }
+
+}
